914533a2440a4a2a5bad2d278eb2e5b1b45a2447
ce441-981-95105849 <m.gorji76@yahoo.com>
2020-08-14 21:59:06 +0430

    sum function added to calculator

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+def calculator_sum(a, b):
+    return a + b
+
+
 operator = input("Enter the operator you need: ")
+
+if operator == "sum":
+    number_one = float(input())
+    number_two = float(input())
+    result = calculator_sum(number_one, number_two)
+    print("result of", operator, "is:", result)
+
+
 
