d0fb5d89d9239930587de8fc8866ead2009ea44c
ce441-981-95105849 <m.gorji76@yahoo.com>
2020-08-14 22:02:20 +0430

    divide function added to calculator

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def calculator_multiply(a, b):
     return a * b
 
 
+def calculator_divide(a, b):
+    return a / b
+
+
 operator = input("Enter the operator you need: ")
 
 if operator == "sum":
@@ -19,5 +23,14 @@ if operator == "multiply":
     number_two = float(input())
     result = calculator_multiply(number_one, number_two)
     print("result of", operator, "is:", result)
+
+if operator == "divide":
+    number_one = float(input())
+    number_two = float(input())
+    if number_two != 0:
+        result = calculator_divide(number_one, number_two)
+        print("result of", operator, "is:", result)
+    else:
+        print("result of", operator, "is:", "Bad input")
 
 
