0c327f6aa752b3805708ab1c6b4472b24eec63b2
ce441-981-95105849 <m.gorji76@yahoo.com>
2020-08-14 22:00:22 +0430

    multiply function added to calculator

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def calculator_sum(a, b):
     return a + b
 
 
+def calculator_multiply(a, b):
+    return a * b
+
+
 operator = input("Enter the operator you need: ")
 
 if operator == "sum":
@@ -10,5 +14,10 @@ if operator == "sum":
     result = calculator_sum(number_one, number_two)
     print("result of", operator, "is:", result)
 
+if operator == "multiply":
+    number_one = float(input())
+    number_two = float(input())
+    result = calculator_multiply(number_one, number_two)
+    print("result of", operator, "is:", result)
 
 
